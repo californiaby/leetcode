1ce1e995f63e9064e432af950e3716ce022a1020
californiaby <californiaby@gmail.com>
2018-05-09 13:34:42 -0400

    Alternative solution for 557 Reverse Words in a String III

--- js/src/557_ Reverse_Words_in_a_String_III/index.js
+++ js/src/557_ Reverse_Words_in_a_String_III/index.js
@@ -1,13 +1,17 @@
 'use strict';
 
+/* eslint-disable no-unused-vars */
+
 let reverseWord = function(word) {
   let rev = '';
   for (let i = word.length - 1; i >= 0; i--) {
     rev += word[i];
+    // String is immutable in JS, implement StringBuilder instead
   }
   return rev;
 };
 
+// First solution - split / reverse / join
 let solution = function(s) {
   if (!s) {
     return '';
@@ -21,4 +25,18 @@ let solution = function(s) {
   .join(' ');
 };
 
-module.exports = (s) => solution(s);
+// alternative - slooow
+let solutionTwo = function(s) {
+  if (!s) {
+    return '';
+  }
+
+  return s
+  .split(' ')
+  .map((word) => {
+    return word.split('').reverse().join('');
+  })
+  .join(' ');
+};
+
+module.exports = (s) => solutionTwo(s);
