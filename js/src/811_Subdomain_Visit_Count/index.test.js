d93032399e672e3331df18fdc666e0189f40d00b
californiaby <californiaby@gmail.com>
2018-05-10 16:45:04 -0400

    Fix test

--- js/src/811_Subdomain_Visit_Count/index.test.js
+++ js/src/811_Subdomain_Visit_Count/index.test.js
@@ -1,8 +1,8 @@
 let solution = require('./index.js');
 
 test('lots', () => {
-  let ans = solution(['900 google.mail.com', '50 yahoo.com', '1 intel.mail.com', '5 wiki.org']);
-  let exp = ['901 mail.com', '50 yahoo.com', '900 google.mail.com', '5 wiki.org', '5 org', '1 intel.mail.com', '951 com'];
+  let ans = solution(['900 google.mail.com']);
+  let exp = ['900 google.mail.com', '900 mail.com', '900 com'];
 
   expect(ans).toEqual(exp);
 });
