c9abef2da0f2abe2eac2b3c247b192a639d03ae8
californiaby <californiaby@gmail.com>
2018-06-10 17:45:14 -0400

    206. Reverse Linked List

---
+++ java/src/test/java/reverse_linked_list_206/SolutionTest.java
@@ -0,0 +1,19 @@
+package reverse_linked_list_206;
+
+import org.junit.Test;
+import utils.ListNode;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+  Solution solution = new Solution();
+
+  @Test
+  public void reverseList() {
+    ListNode list = new ListNode(1).add(2).add(3);
+    ListNode exp = new ListNode(3).add(2).add(1);
+
+    assertEquals(exp.toString(), solution.reverseList(list).toString());
+  }
+}
