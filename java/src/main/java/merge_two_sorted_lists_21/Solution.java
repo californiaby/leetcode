57abad4420412d053c1ffa3735afbaccb1be6e92
californiaby <californiaby@gmail.com>
2018-06-10 18:24:08 -0400

    21. Merge Two Sorted Lists

---
+++ java/src/main/java/merge_two_sorted_lists_21/Solution.java
@@ -0,0 +1,24 @@
+package merge_two_sorted_lists_21;
+
+import utils.ListNode;
+
+public class Solution {
+
+  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+    // 1 > 2 > 4
+    // 1 > 3 > 4
+
+    if (l1 == null) return l2;
+    if (l2 == null) return l1;
+
+    if (l1.val < l2.val) {
+      l1.next = mergeTwoLists(l1.next, l2);
+      return l1;
+    } else {
+      l2.next = mergeTwoLists(l1, l2.next);
+      return l2;
+    }
+  }
+
+}
---
+++ java/src/test/java/merge_two_sorted_lists_21/SolutionTest.java
@@ -0,0 +1,20 @@
+package merge_two_sorted_lists_21;
+
+import org.junit.Test;
+import utils.ListNode;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+  Solution solution = new Solution();
+
+  @Test
+  public void mergeTwoLists() {
+    ListNode l1 = new ListNode(1).add(2).add(6);
+    ListNode l2 = new ListNode(1).add(3).add(4);
+    ListNode expected = new ListNode(1).add(1).add(2).add(3).add(4).add(6);
+
+    assertEquals(expected.toString(), solution.mergeTwoLists(l1, l2).toString());
+  }
+}
