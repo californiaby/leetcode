640bf96693807e268d7b0603f03493b215762831
californiaby <californiaby@gmail.com>
2018-06-23 15:46:27 -0400

    Book: Tree successor

--- java/src/main/java/book/TreeSuccessor.java
+++ java/src/main/java/book/TreeSuccessor.java
@@ -9,24 +9,24 @@ import utils.TreeNode;
 
 public class TreeSuccessor {
 
-  public TreeNode getSuccessor(TreeNode n) {
-
-    if (n == null) return null;
-
-    // if there's right branch - get the leftmost element of it
-    if (n.right != null) {
-      return leftmost(n.right);
-    } else {
-      TreeNode q = n;
-      TreeNode x = q.parent;
-
-      // Go up until we are on left instead of right
-      while (x != null && x.left != q) {
-        q = x;
-        x = x = q.parent;
-      }
-      return x;
-    }
-  }
+//  public TreeNode getSuccessor(TreeNode n) {
+//
+//    if (n == null) return null;
+//
+//    // if there's right branch - get the leftmost element of it
+//    if (n.right != null) {
+//      return leftmost(n.right);
+//    } else {
+//      TreeNode q = n;
+//      TreeNode x = q.parent;
+//
+//      // Go up until we are on left instead of right
+//      while (x != null && x.left != q) {
+//        q = x;
+//        x = x = q.parent;
+//      }
+//      return x;
+//    }
+//  }
 
 }
