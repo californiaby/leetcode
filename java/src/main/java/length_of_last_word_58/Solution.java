bc8a9e461fbd7180966f03ebbf63ae0ed990b5b8
californiaby <californiaby@gmail.com>
2018-06-26 08:21:08 -0400

    58. Length of Last Word

--- java/src/main/java/length_of_last_word_58/Solution.java
+++ java/src/main/java/length_of_last_word_58/Solution.java
@@ -1,4 +1,24 @@
 package length_of_last_word_58;
 
 public class Solution {
+
+  /*
+  Given a string s consists of upper/lower-case alphabets and empty space characters ' ',
+  return the length of last word in the string.
+  If the last word does not exist, return 0.
+  Note: A word is defined as a character sequence consists of non-space characters only
+   */
+
+  public int lengthOfLastWord(String s) {
+
+    String[] arr = s.split("\\s+");
+
+    if (arr.length < 1) {
+      return 0;
+    } else {
+      return arr[arr.length - 1].length();
+    }
+
+  }
+
 }
--- java/src/test/java/length_of_last_word_58/SolutionTest.java
+++ java/src/test/java/length_of_last_word_58/SolutionTest.java
@@ -1,5 +1,34 @@
+package length_of_last_word_58;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SolutionTest {
 
+  Solution solution = new Solution();
+
+  @Test
+  public void lengthOfLastWord() {
+    String s = "Hello world!";
+    assertEquals(6, solution.lengthOfLastWord(s));
+  }
+
+  @Test
+  public void test_empty() {
+    String s = "";
+    assertEquals(0, solution.lengthOfLastWord(s));
+  }
+
+  @Test
+  public void test_no_spaces() {
+    String s = "zxc";
+    assertEquals(3, solution.lengthOfLastWord(s));
+  }
+
+  @Test
+  public void test_only_spaces() {
+    String s = "   ";
+    assertEquals(0, solution.lengthOfLastWord(s));
+  }
 }
