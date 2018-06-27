a8898d64a63fa6716a03d9c09b7a7858dd81c84e
californiaby <californiaby@gmail.com>
2018-06-27 16:57:18 -0400

    20. Valid Parentheses

---
+++ java/src/main/java/valid_parentheses_20/Solution.java
@@ -0,0 +1,44 @@
+package valid_parentheses_20;
+
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+ */
+
+import java.util.Stack;
+
+public class Solution {
+
+  public boolean isValid(String s) {
+
+    if (s.length() == 0) return true;
+
+    Stack<Character> stack = new Stack<>();
+
+    // we can use stack to store values
+    // we can also add "inverted" value to stack to make comparison simpler
+
+    for (char c : s.toCharArray()) {
+
+      // if "opening" char - add corresponding "closing" to the stack
+      // otherwise - pop top and compare to current
+      if (c == '(') {
+        stack.push(')');
+      } else if (c == '{') {
+        stack.push('}');
+      } else if (c == '[') {
+        stack.push(']');
+      } else if (stack.isEmpty() || stack.pop() != c) {
+        return false;
+      }
+    }
+    return stack.isEmpty();
+  }
+
+}
---
+++ java/src/test/java/valid_parentheses_20/SolutionTest.java
@@ -0,0 +1,46 @@
+package valid_parentheses_20;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+  Solution solution = new Solution();
+
+  @Test
+  public void test_valid() {
+    String s = "{{[[(())]]}}";
+    assertTrue(solution.isValid(s));
+  }
+
+  @Test
+  public void valid_sequence() {
+    String s = "{}()[(){{}}]";
+    assertTrue(solution.isValid(s));
+  }
+
+  @Test
+  public void test_wrong_order() {
+    String s = "{[}]";
+    assertFalse(solution.isValid(s));
+  }
+
+  @Test
+  public void test_non_equal() {
+    String s = "{{{()}}";
+    assertFalse(solution.isValid(s));
+  }
+
+  @Test
+  public void test_empty() {
+    String s = "";
+    assertTrue(solution.isValid(s));
+  }
+
+  @Test
+  public void starts_with_closing() {
+    String s = "}{";
+    assertFalse(solution.isValid(s));
+  }
+}
