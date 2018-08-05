7584cb51e6915929d546398936d7fbb81c2ba55e
californiaby <californiaby@gmail.com>
2018-08-04 21:26:29 -0400

    238. Product of Array Except Self

--- java/src/main/java/Product_of_Array_Except_Self_238/Solution.java
+++ java/src/main/java/Product_of_Array_Except_Self_238/Solution.java
@@ -12,18 +12,19 @@ Note: Please solve it without division and in O(n).
 public class Solution {
 
   public int[] productExceptSelf(int[] nums) {
-    int n = nums.length;
-    int[] res = new int[n];
-    res[0] = 1;
-    for (int i = 1; i < n; i++) {
-      res[i] = res[i - 1] * nums[i - 1];
+    int[] result = new int[nums.length]
+            ;
+    for (int i = 0, tmp = 1; i < nums.length; i++) {
+      result[i] = tmp;
+      tmp *= nums[i];
     }
-    int right = 1;
-    for (int i = n - 1; i >= 0; i--) {
-      res[i] *= right;
-      right *= nums[i];
+
+    for (int i = nums.length - 1, tmp = 1; i >= 0; i--) {
+      result[i] *= tmp;
+      tmp *= nums[i];
     }
-    return res;
+    
+    return result;
   }
 
 }
